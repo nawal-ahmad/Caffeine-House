6a79eef33006386d1238fe224c1a04d52f335622
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-04-04 17:57:42 +0300

    addind css and javascript

---
+++ Javascript/app.js
@@ -0,0 +1,22 @@
+let userName= prompt('what\'s your name?');
+
+let userAge= prompt('How old are you?');
+
+let favDrink= prompt('what\'s your favorite drink','tea , cola , coffee');
+
+let cupsNum= prompt ('How many cups do you have daily?(250m;l)')
+
+
+alert('Welcome to the Caffeine House '+ userName+ ' ,you are ' + userAge + ' years old, and your favorite drink is '+ favDrink);
+
+let cupsNumber = parseInt(cupsNum);
+console.log(userName);
+console.log(userAge);
+console.log(favDrink);
+console.log (cupsNum);
+
+console.log(typeof(userName));
+console.log(userAge);
+console.log(favDrink);
+console.log (typeof(cupsNum));
+console.log (typeof(cupsNumber));
